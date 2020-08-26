1369de395630c1f21eb9267f659421573cf5d6ba
Eliezer <emmveli49@gmail.com>
2020-08-26 15:10:40 +0200

    Initialization

---
+++ MyFunTranslations/MyFunTranslations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFunTranslations
+//
+//  Created by admin on 2020/08/26.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
