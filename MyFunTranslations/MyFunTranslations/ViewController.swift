a06870f81ef4d9f950c93e53b9ba6e285edbb586
Eliezer <emmveli49@gmail.com>
2020-08-27 13:55:54 +0200

    Updated UI + Connected PickerView

--- MyFunTranslations/MyFunTranslations/ViewController.swift
+++ MyFunTranslations/MyFunTranslations/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var picker:UIPickerView!
+    @IBOutlet weak var textField:UITextField!
+    
+    let pickerData = ["English to Morse", "Morse to English", "US to UK", "UK to US", "English to Shakespeare"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.picker.delegate = self
+        self.picker.dataSource = self
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
 
 }
 
