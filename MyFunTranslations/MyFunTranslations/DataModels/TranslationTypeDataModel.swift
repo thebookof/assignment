602db5e0f06c716c0ad9546d55119b6444fa3b26
Eliezer <emmveli49@gmail.com>
2020-09-01 14:13:43 +0200

    final refactor

---
+++ MyFunTranslations/MyFunTranslations/DataModels/TranslationTypeDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  TranslationTypeDataModel.swift
+//  MyFunTranslations
+//
+//  Created by admin on 2020/09/01.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+struct TranslationPickerDataModel {
+    
+    let translationPickerData = ["US to UK", "UK to US", "English to Morse", "English to Old English", "English to Shakespeare", "English to Pig Latin", "English to Dothraki",
+     "English to Yoda Speak", "English to Sith Speak", "English to Startrek Vulcan Language", "English to Startrek Klingon Language"]
+}
--- MyFunTranslations/MyFunTranslations/ViewControllers/MainViewController.swift
+++ MyFunTranslations/MyFunTranslations/ViewControllers/MainViewController.swift
@@ -13,8 +13,9 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var textView: UITextView!
     
+    let translationPickerDataModel = TranslationPickerDataModel()
+    
     var translationType = "us2uk"
-    let pickerData = ["US to UK", "UK to US", "English to Morse", "English to Old English", "English to Shakespeare"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +28,14 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
+        return self.translationPickerDataModel.translationPickerData.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
+        return self.translationPickerDataModel.translationPickerData[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -64,6 +66,12 @@ enum SelectedTranslationType: Int {
     case morse
     case oldenglish
     case shakespeare
+    case piglatin
+    case dothraki
+    case yoda
+    case sith
+    case vulcan
+    case klingon
     
     var name: String {
         get {return String(describing: self)}
